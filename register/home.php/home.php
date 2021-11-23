ae0747a3bfa3c11a3f0033f7c67d515b1cd92d6f
sucipto345 <94429817+sucipto345@users.noreply.github.com>
2021-11-23 15:27:44 +0800

    tired
    i so tired and have many problem i can fix it tomorrow mybe

---
+++ register/home.php/home.php
@@ -0,0 +1,4 @@
+<div class="page">
+    <h2>Website Hebat Berawal dari Unlimited Hosting</h2>
+    <p>Kami berikan penawaran terbaik unlimited web hosting. Fitur terlengkap, harga terjangkau, dan dukungan teknis 24/7 telah tersedia untuk Anda.</p>
+</div>
